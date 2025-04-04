4da86f6dec69c222e461ccc8eb9f11dcd93b260e
nesaratai <nesaratai@gmail.com>
2025-04-03 20:40:56 -0400

    new

--- server.js
+++ server.js
@@ -5,14 +5,23 @@ dotenv.config(); // Loads the environment variables from .env file
 // We begin by loading Express
 const express = require('express');
 const mongoose = require("mongoose"); // require package
-
+const methodOverride = require("method-override"); // new
+const morgan = require("morgan"); //new
 
 
 
 const app = express();
+
+
+// Add Middleware
 app.use(express.urlencoded({ extended: false }));
+// Mount it along with our other middleware, ABOVE the routes
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
 
 
+//connect to mongo db mongooose
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -29,6 +38,41 @@ app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
   });
 
+  
+  // GET localhost:3000/fruits/:fruitId/edit
+  app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/edit.ejs", {
+      fruit: foundFruit,
+    });
+  });
+  
+
+app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+  });
+  
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+  });
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
+  });
+  
 app.post("/fruits", async (req, res) => {
 
     console.log("Before",req.body)
